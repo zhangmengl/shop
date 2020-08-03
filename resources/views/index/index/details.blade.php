65a5f0ac0adcce069aa2d1c0afa80a0a036c1063
zhangmengli <2402784619@qq.com>
2020-08-03 16:09:06 +0800

    更新商品详情

--- resources/views/index/index/details.blade.php
+++ resources/views/index/index/details.blade.php
@@ -179,26 +179,6 @@
 
 </script>
 
-<!-- footer -->
-<div class="footer">
-    <div class="container">
-        <div class="about-us-foot">
-            <h6>Mstore</h6>
-            <p>is a lorem ipsum dolor sit amet, consectetur adipisicing elit consectetur adipisicing elit.</p>
-        </div>
-        <div class="social-media">
-            <a href=""><i class="fa fa-facebook"></i></a>
-            <a href=""><i class="fa fa-twitter"></i></a>
-            <a href=""><i class="fa fa-google"></i></a>
-            <a href=""><i class="fa fa-linkedin"></i></a>
-            <a href=""><i class="fa fa-instagram"></i></a>
-        </div>
-        <div class="copyright">
-            <span>© 2017 All Right Reserved</span>
-        </div>
-    </div>
-</div>
-<!-- end footer -->
 
 <!-- scripts -->
 <script src="js/jquery.min.js"></script>
